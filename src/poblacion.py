a59de88a2ad97a251b60772c6b71f286fd14f642
KNX7390 <marrojrod1@alum.us.es>
2024-10-23 08:27:16 +0200

    Ejercicio completado

--- src/poblacion.py
+++ src/poblacion.py
@@ -1,5 +1,6 @@
 import csv 
 from collections import namedtuple
+import matplotlib.pyplot as plt
 RegistroPoblacion = namedtuple('RegistroPoblacion', 'pais, codigo, año, censo')
 def lee_poblaciones(ruta_fichero): 
     resultado=[]
@@ -11,12 +12,57 @@ def lee_poblaciones(ruta_fichero):
             tupla=RegistroPoblacion(pais,codigo,año,censo)
             resultado.append(tupla)
     return resultado
-def calcula_paises(registro_poblacion): 
+def calcula_paises(poblaciones): 
     lista_pueblos=set()
-    for pueblo in registro_poblacion:
+    for pueblo in poblaciones:
         lista_pueblos.add(pueblo.pais)
-        conjunto_paises=list(lista_pueblos)
-        conjunto_paises.sort()
-    return conjunto_paises
-def filtra_por_pais(poblaciones,nombre_o_codigo): 
-    for 
+        lista_paises=list(lista_pueblos)
+        lista_paises.sort()
+    return lista_paises    
+
+def filtra_por_pais(poblacion,nombre_o_codigo):
+    res = []
+    for pais in poblacion:
+           if pais.codigo == nombre_o_codigo or pais.pais == nombre_o_codigo:
+               res.append((pais.año, pais.censo))
+    return res
+def filtrar_por_paises_y_año(poblaciones,año,paises): 
+    tupla=[] 
+    for variable in poblaciones:
+        if variable.pais in paises and variable.año==año:  
+            tupla.append((variable.pais,variable.censo))
+    return tupla
+
+def muestra_evolucion_poblacion(poblaciones,nombre_o_codigo):
+    titulo="Esta es la gráfica que representa la evolución de la poblacion"
+    lista_años=[]
+    lista_habitantes=[]
+    for i in poblaciones:
+        if i.codigo==nombre_o_codigo or i.pais==nombre_o_codigo: 
+            lista_años.append(i.año)
+            lista_habitantes.append(i.censo)
+    plt.title(titulo)
+    plt.plot(lista_años, lista_habitantes)
+    plt.show()
+
+def muestra_comparativa_paises_año(poblaciones,año,paises): 
+    titulo="Esta es la gráfica que compara la evolución de la población"
+    lista_paises=sorted([])
+    lista_habitantes=[]
+    paises=set(paises)
+    for var in poblaciones: 
+            for pais in paises:
+                if pais==var.pais and año==var.año: #problemas con el operador
+                    lista_paises.append(var.pais)
+                    lista_habitantes.append(var.censo)
+    plt.title(titulo)
+    plt.bar(lista_paises, lista_habitantes)
+    plt.show()
+
+        
+        
+
+        
+
+     
+       
--- src/poblacion_test.py
+++ src/poblacion_test.py
@@ -10,11 +10,32 @@ def test_lee_poblaciones(registro):
     print("==> Mostramos los 4 últimos datos:")
     imprime_datos_tabulados(registro[-4:])
 
-def test_calcula_paises(conjunto_paises): 
-    print(f"==>Aquí tenemos una lista ordenada con los nombres de los países: {conjunto_paises}")
-    print (f"==>Se han leído {len(conjunto_paises)}")
+def test_calcula_paises(lista_paises): 
+    print(f"==>Aquí tenemos una lista ordenada con los nombres de los países: {lista_paises}")
+    print (f"==>Se han leído {len(lista_paises)}")
+
+def test_filtra_por_paises(poblaciones): 
+    print(f"==>Aquí se muestran los datos del año y el censo: {poblaciones}")
+
+def test_filtrar_por_paises_y_año(paises_y_año): 
+    print(f"Se muestra la población del país en el año indicado: {paises_y_año}")
+
+def test_muestra_evolucion_poblacion(grafica): 
+    print(grafica)
+
+def test_muestra_comparativa_paises_año(graf2): 
+    print(graf2)
 if __name__=='__main__': 
     datos=lee_poblaciones('data/population.csv')
     test_lee_poblaciones(datos)
-    conjunto_paises=calcula_paises(datos)
-    test_calcula_paises(conjunto_paises)
+    lista_paises=calcula_paises(datos)
+    test_calcula_paises(lista_paises)
+    poblaciones=filtra_por_pais(datos,"Zimbabwe")
+    test_filtra_por_paises(poblaciones)
+    paises_y_año=filtrar_por_paises_y_año(datos,2000,"Arab World, Zimbabwe, East Asia & Pacific")
+    test_filtrar_por_paises_y_año(paises_y_año)
+    grafica=muestra_evolucion_poblacion(datos,"Zimbabwe")
+    test_muestra_evolucion_poblacion(grafica)
+    graf2=muestra_comparativa_paises_año(datos,1960,("Central Europe and the Baltics","Arab World","Caribbean small states","Early-demographic dividend","East Asia & Pacific"))
+    test_muestra_comparativa_paises_año(graf2)
+    
